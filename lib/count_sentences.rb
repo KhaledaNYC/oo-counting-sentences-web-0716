925adc63b6bbe5c236f761b82c79e3f57c666ec2
Khaleda Khan <khaleda.khan87@gmail.com>
2016-07-28 13:49:21 +0000

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
+  attr_accessor :sentence
+
+  def initialize(sentence)
+    @sentence = sentence
+  end
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\b?\.\s|\?\s|\!\s/).size
   end
 end
